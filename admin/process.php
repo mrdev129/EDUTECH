8cc5175b28df10f53d1b529e7bf2ba975053044a
mrdev129 <debabratabehera129@gmail.com>
2026-03-08 00:04:46 +0530

    secure credentials

--- admin/process.php
+++ admin/process.php
@@ -1,24 +1,16 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
-$SMTP_HOST = $_ENV['SMTP_HOST'];
-$SMTP_USER = $_ENV['SMTP_USER'];
-$SMTP_PASS = $_ENV['SMTP_PASS'];
-$SMTP_PORT = $_ENV['SMTP_PORT'];
-
-session_start();
-include '../config/db.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
-require '../PHPMailer/src/Exception.php';
+// Load .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
+session_start();
+include '../config/db.php';
 
 $username = mysqli_real_escape_string($conn,$_POST['username']);
 $password = md5($_POST['password']);
@@ -33,27 +25,23 @@ if(mysqli_num_rows($query)==1){
     $_SESSION['admin_id'] = $admin['id'];
     $_SESSION['admin_email'] = $admin['email'];
 
-    // GENERATE OTP
     $otp = rand(100000,999999);
     $_SESSION['login_otp'] = $otp;
 
-    // SEND EMAIL
     $mail = new PHPMailer(true);
 
     $mail->isSMTP();
-    // $mail->Host = 'smtp.gmail.com';
-    // $mail->SMTPAuth = true;
-    // $mail->Username = 'debabratabehera437@gmail.com';
-    // $mail->Password = 'hdttdhprcodrfdji';
-    // $mail->SMTPSecure = 'tls';
-    // $mail->Port = 587;
+    $mail->Host = $_ENV['SMTP_HOST'];
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['SMTP_USER'];
+    $mail->Password = $_ENV['SMTP_PASS'];
+    $mail->SMTPSecure = $_ENV['SMTP_SECURE'];
+    $mail->Port = $_ENV['SMTP_PORT'];
 
-    $mail->setFrom('debabratabehera437@gmail.com','EDUTECH Admin');
-
+    $mail->setFrom($_ENV['SMTP_USER'],'EDUTECH Admin');
     $mail->addAddress($admin['email']);
 
     $mail->Subject = 'Admin Login OTP';
-
     $mail->Body = "Your OTP is: ".$otp;
 
     $mail->send();
@@ -65,5 +53,3 @@ if(mysqli_num_rows($query)==1){
     echo "<script>alert('Invalid username or password');window.location='login.php';</script>";
 
 }
-
-?>
