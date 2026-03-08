3e79787e49d1c1f0057304241bfb0f4bd4acbac9
mrdev129 <debabratabehera129@gmail.com>
2026-03-08 12:06:08 +0530

    secure credentials

---
+++ admin/otp.php
@@ -0,0 +1,30 @@
+<div class="wrapper">
+
+<div class="login-card">
+
+<div class="card-header">
+<h4>OTP Verification</h4>
+</div>
+
+<div class="card-body">
+
+<form action="verify_otp.php" method="POST">
+
+<div class="input-group">
+<input type="text" name="otp" class="form-control" placeholder="Enter OTP">
+<span class="input-group-text"><i class="fa-solid fa-key"></i></span>
+</div>
+
+<br>
+
+<button type="submit" class="btn btn-login w-100">
+Verify OTP
+</button>
+
+</form>
+
+</div>
+
+</div>
+
+</div>
